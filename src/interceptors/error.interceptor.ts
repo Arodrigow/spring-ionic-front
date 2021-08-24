0eb59cb4fc17702afad3076e11a28259a20e3968
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-08-24 10:39:38 -0300

    [Update] Using alert to handle default exceptions

--- src/interceptors/error.interceptor.ts
+++ src/interceptors/error.interceptor.ts
@@ -2,10 +2,11 @@ import { StorageService } from './../services/storage.service';
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HTTP_INTERCEPTORS } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs/Rx";
+import { AlertController } from 'ionic-angular';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-  constructor(public storage: StorageService) {}
+  constructor(public storage: StorageService, public alertControl: AlertController) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req)
@@ -22,9 +23,14 @@ export class ErrorInterceptor implements HttpInterceptor {
         console.log(errorObj)
 
         switch (errorObj.status) {
+          case 401:
+            this.handle401();
+            break;
           case 403:
             this.handle403();
             break;
+          default:
+            this.handleDefaultError(errorObj);
         }
 
         return Observable.throw(errorObj);
@@ -34,6 +40,31 @@ export class ErrorInterceptor implements HttpInterceptor {
   handle403() {
     this.storage.setLocalUser(null);
   }
+
+  handle401() {
+    const alert = this.alertControl.create({
+      title: 'Error 401: authentication failed',
+      message: 'Email or password is incorrect',
+      enableBackdropDismiss: false,
+      buttons: [{
+        text: 'ok',
+      }]
+    });
+    alert.present();
+  }
+
+  handleDefaultError(errorObj) {
+    const alert = this.alertControl.create({
+      title: `Error ${errorObj.status}: ${errorObj.error}`,
+      message: errorObj.message,
+      enableBackdropDismiss: false,
+      buttons: [{
+        text: 'ok',
+      }]
+    });
+    alert.present();
+  }
+
 }
 
 export const ErrorInteceptorProvider = {
