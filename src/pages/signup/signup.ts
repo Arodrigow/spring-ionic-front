da8036acc33debb1bed42b2be6eeee6ee3024e5b
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-08-24 11:45:55 -0300

    [Added] FormGroup to control signup form

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
@@ -8,7 +9,26 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class SignupPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  formGroup: FormGroup
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group({
+      name: ['Oy', [Validators.required, Validators.minLength(5), Validators.maxLength(120)]],
+      email: ['oy@gmail.com', [Validators.required, Validators.email]],
+      type: ['1', [Validators.required]],
+      cpfOrCnpj: ['99406311038', [Validators.required, Validators.minLength(11), Validators.maxLength(15)]],
+      password: ['123', [Validators.required]],
+      publicPlace: ['Rua João Gentilini Fasciani', [Validators.required]],
+      number: ['219', [Validators.required]],
+      complement: ['Perto da esquina', []],
+      district: ['Fátima', []],
+      cep: ['39800000', [Validators.required]],
+      phone1: ['33988881111', [Validators.required]],
+      phone2: ['', []],
+      phone3: ['', []],
+      stateId: [null, [Validators.required]],
+      cityId: [null, [Validators.required]]
+    });
   }
 
   ionViewDidLoad() {
@@ -18,4 +38,6 @@ export class SignupPage {
   signupUser() {
     console.log("form");
   }
+
+
 }
