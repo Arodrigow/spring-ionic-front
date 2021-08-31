6eddafe761bdf2671d1569e759e4e817c80553da
André Rodrigo B. A. Maciel <rodrigow_@hotmail.com.br>
2021-08-31 14:48:42 -0300

    [Update] Using backend url

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-  baseUrl: "http://localhost:8080",
+  baseUrl: "https://spring-ionic-mobile.herokuapp.com",
   bucketBaseUrl: "https://spring-ionic-rdw.s3.sa-east-1.amazonaws.com"
 }
